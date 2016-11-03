24ad1df71afccf0c2a1ffb8ef89978d1123e4430
cyanide4all <marvazto@gmail.com>
2016-11-03 19:10:05 +0100

    Primer commit

---
+++ SharpAgenda/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+namespace Grafico
+{
+	public class MainClass
+	{
+		public static void Main (String[] args)
+		{
+			/*//Test de contactos
+			Agenda hue = Agenda.Get ();
+			Console.Write (hue.ToString ());
+			hue.AddContacto(new Contacto("hue","hue lelelele","huehue","huhue","678856329"));
+			hue.AddContacto ("hue2", "hue2 lelelele2", "huehue2", "huhue2", "678856322");
+			hue.SaveXML ();
+			*///END Test de contactos
+
+
+
+			Application.Init ();
+			new VentanaGrafico ();
+			Application.Run ();
+		}
+
+	}
+}
---
+++ SharpAgenda/Meetings/Cita.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Meetings
+{
+	public class Cita
+	{
+		public Cita (string nombre, string nomContacto, string fecha, string hora, string des)
+		{
+			Nombre = nombre;
+			NombreContacto = nomContacto;
+			Fecha = fecha;
+			Hora = hora;
+			Descripcion = des;
+		}
+
+		public string Nombre 
+		{
+			get; private set;
+		}
+
+		public string NombreContacto 
+		{
+			get; private set;
+		}
+
+		public string Fecha 
+		{
+			get; private set;
+		}
+
+		public string Hora 
+		{
+			get; private set;
+		}
+
+		public string Descripcion 
+		{
+			get; private set;
+		}
+
+		public string ToString (int actual)
+		{
+			return string.Format ("Cita#{0}:\nNombre={1}\nNombreContacto={2}\nFecha={3}\nHora={4}\nDescripcion={5}", actual, Nombre, NombreContacto, Fecha, Hora, Descripcion );
+		}
+	}
+}
+
