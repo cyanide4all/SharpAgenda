0977079de00d32371ee91914baa9861003d3af69
LuisCadavid <lbcadavid@esei.uvigo.es>
2016-11-03 19:20:15 +0100

    Parte añadida

---
+++ SharpAgenda-master/SharpAgenda/Contactos/Contacto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Contactos
+{
+	public class Contacto
+	{
+		public string Nombre {
+			get;
+			private set;
+		}
+		public string Apellidos {
+			get;
+			private set;
+		}
+		public string Direccion {
+			get;
+			private set;
+		}
+		public string Email {
+			get;
+			private set;
+		}
+		public string Telefono {
+			get;
+			private set;
+		}
+
+		public Contacto ()
+		{
+		}
+		public Contacto (String n, String a, String d, String e, String t)
+		{
+			Nombre = n;
+			Apellidos = a;
+			Direccion = d;
+			Email = e;
+			Telefono = t;
+		}
+		public override string ToString ()
+		{
+			return string.Format ("[Contacto: Nombre={0} \nApellidos={1} \nDireccion={2} \nEmail={3} \nTelefono={4}]", Nombre, Apellidos, Direccion, Email, Telefono);
+		}
+
+
+	}
+}
+
