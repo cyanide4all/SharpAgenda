c0d3a1804c6ea7b68d3928a45ef66d81b01a12f6
cyanide4all <marvazto@gmail.com>
2016-11-10 19:44:05 +0100

    Trabajando en ventana proncipal y menus

--- SharpAgenda/UI/VentanaPrincipal.cs
+++ SharpAgenda/UI/VentanaPrincipal.cs
@@ -11,12 +11,36 @@ namespace SharpAgenda
 			:base("SharpAgenda")
 		{
 			//Config
-			BorderWidth = 15;
+			BorderWidth = 10;
+			SetDefaultSize(250, 200);
+			SetPosition(WindowPosition.Center);
+			DeleteEvent += delegate { Application.Quit(); };
+
+			//Partes
+			MenuBar mb = new MenuBar();
+			Menu contactosOpciones = new Menu ();
+			MenuItem contactosMenu = new MenuItem ("Contactos");
+			MenuItem contactosListar = new MenuItem ("Listar contactos");
+			contactosMenu.Submenu = contactosOpciones;
 
 
-			Main.Add(new Label("Bienvenido a SharpAgenda\nSelecciona una opcion usando el menu"));
-			this.Add (Main);
-			this.ShowAll();
+			contactosOpciones.Append (contactosListar);
+			mb.Append(contactosMenu);
+
+
+			//PASTEADO
+			VBox menuBox = new VBox(false, 2);
+			menuBox.PackStart(mb, false, false, 0);
+
+			VBox textBox = new VBox ();
+
+			textBox.Add(new Label("Bienvenido a SharpAgenda\nSelecciona una opcion usando el menu"));
+			Main.Add (menuBox);
+			Main.Add (textBox);
+
+			Add (Main);
+
+			ShowAll();
 
 
 
