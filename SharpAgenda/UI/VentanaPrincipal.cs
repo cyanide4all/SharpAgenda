10bde9caeeff13b9e3a3bd1ee4f0f5ac6d9d6552
cyanide4all <marvazto@gmail.com>
2016-12-15 16:53:30 +0100

    Haciendo menuses

--- SharpAgenda/UI/VentanaPrincipal.cs
+++ SharpAgenda/UI/VentanaPrincipal.cs
@@ -18,15 +18,50 @@ namespace SharpAgenda
 
 			//Partes
 			MenuBar mb = new MenuBar();
+
 			Menu contactosOpciones = new Menu ();
 			MenuItem contactosMenu = new MenuItem ("Contactos");
 			MenuItem contactosListar = new MenuItem ("Listar contactos");
-			contactosMenu.Submenu = contactosOpciones;
+			MenuItem contactosCrear = new MenuItem ("Crear Contacto");
+			MenuItem contactosBorrar = new MenuItem ("Borrar contactos");
+			MenuItem contactosModificar = new MenuItem ("Modificar contactos");
 
+			Menu citasOpciones = new Menu ();
+			MenuItem citasMenu = new MenuItem ("Citas");
+			MenuItem citasListar = new MenuItem ("Listar citas");
+			MenuItem citasCrear = new MenuItem ("Crear citas");
+			MenuItem citasBorrar = new MenuItem ("Borrar citas");
+			MenuItem citasModificar = new MenuItem ("Modificar citas");
+
+			Menu notasOpciones = new Menu ();
+			MenuItem notasMenu = new MenuItem ("Notas");
+			MenuItem notasListar = new MenuItem ("Listar notas");
+			MenuItem notasCrear = new MenuItem ("Crear notas");
+			MenuItem notasBorrar = new MenuItem ("Borrar notas");
+			MenuItem notasModificar = new MenuItem ("Modificar notas");
+
+			contactosMenu.Submenu = contactosOpciones;
+			citasMenu.Submenu = citasOpciones;
+			notasMenu.Submenu = notasOpciones;
 
 			contactosOpciones.Append (contactosListar);
-			mb.Append(contactosMenu);
+			contactosOpciones.Append (contactosCrear);
+			contactosOpciones.Append (contactosBorrar);
+			contactosOpciones.Append (contactosModificar);
 
+			citasOpciones.Append (citasListar);
+			citasOpciones.Append (citasCrear);
+			citasOpciones.Append (citasBorrar);
+			citasOpciones.Append (citasModificar);
+
+			notasOpciones.Append (notasListar);
+			notasOpciones.Append (notasCrear);
+			notasOpciones.Append (notasBorrar);
+			notasOpciones.Append (notasModificar);
+
+			mb.Append (contactosMenu);
+			mb.Append (citasMenu);
+			mb.Append (notasMenu);
 
 			//PASTEADO
 			VBox menuBox = new VBox(false, 2);
