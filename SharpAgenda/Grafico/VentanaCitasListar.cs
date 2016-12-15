caa58129f335040f2c5c11726b2c206b3ab6ffbc
Alex <achenriques@esei.uvigo.es>
2016-12-15 20:22:35 +0100

    Add files via upload

---
+++ SharpAgenda/Grafico/VentanaCitasListar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Gtk;
+using Calendario;
+using Meetings;
+
+namespace SharpAgenda
+{
+	public class VentanaCitasListar: Window
+	{
+		private Controler_Meetings citas;
+		private String listado;
+
+		public VentanaCitasListar(): base( "Listar Citas" )
+		{
+			citas = new Controler_Meetings ();
+			listado = citas.ToString ();
+			this.Build();
+		}
+
+		private void Build() 
+		{
+			SetDefaultSize(250, 200);
+			SetPosition(WindowPosition.Center);
+			VBox Main = new VBox ();
+			//DeleteEvent += delegate { Application.Quit(); };
+
+			if (listado == "") 
+			{
+				Label aMostrar = new Label ("No hay ninguna cita guardada.");
+				Main.Add(aMostrar);
+			} 
+			else 
+			{
+				System.Console.Write (listado);
+				Label aMostrar = new Label(listado);
+				Main.Add(aMostrar);
+			}
+
+			Add(Main);
+			this.ShowAll ();
+		}
+	}
+}
+
